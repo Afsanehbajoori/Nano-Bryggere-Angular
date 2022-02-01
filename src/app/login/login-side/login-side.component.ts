7e8aee182413c0c5bf9d429ad936c5e86ed98295
Gravhund <57315146+Gravhund-source@users.noreply.github.com>
2022-02-01 14:20:27 +0100

    login navigation tilføjet

--- src/app/login/login-side/login-side.component.ts
+++ src/app/login/login-side/login-side.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Bruger } from 'src/app/Models/Bruger';
 import { Rolle } from 'src/app/Models/Rolle';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RestApiService } from 'src/app/shared/rest-api.service';
 import { Login } from 'src/app/Models/Login';
 
@@ -14,12 +14,14 @@ import { Login } from 'src/app/Models/Login';
 export class LoginSideComponent implements OnInit {
   logins: Bruger
   roller: Rolle
-
+  login: any = [];
+  id = this.actRoute.snapshot.params['id'];
   loginForm : FormGroup;
   @Input() loginDetails = {Brugernavn: ''}
   constructor(
     public router: Router,
-    public restApi: RestApiService
+    public restApi: RestApiService,
+    public actRoute: ActivatedRoute
     ) { }
 
   ngOnInit(): void {
@@ -29,9 +31,34 @@ export class LoginSideComponent implements OnInit {
     }
     );
   }
-  addLogin(datalogin: Login){
-    this.restApi.createLogins(this.loginDetails).subscribe((data: {}) => {
-      this.router.navigate(['../main/main'])
-    })
-  }
+  onSubmitLogin () {
+    this.router.navigate(['../main/main']);
+  };
+  onSubmitRegistre () {
+    this.router.navigate(['../login/registrer']);
+  };
+  // addLogin(datalogin: Login){
+  //   this.restApi.createLogins(this.loginDetails).subscribe((data: {}) => {
+  //     this.router.navigate(['../main/main'])
+  //   })
+  // }
+  // loadLogin(){
+  //   return this.restApi.getLogin().subscribe((data: {}) => {
+  //     this.login = data;
+  //   })
+  // }
+  // deleteLogin(id : any){
+  //   if(window.confirm('Are you sure, you want to delete?')){
+  //     this.restApi.deleteLogin(id).subscribe(data => {
+  //       this.loadLogin()
+  //     })
+  //   }  
+  // }
+  // updateLogin(){
+  //   if(window.confirm('Are you sure, you want to update?')){
+  //     this.restApi.updateLogin(this.id, this.login).subscribe(data => {
+  //       this.router.navigate(['../main/main'])
+  //     })
+  //   }
+  // }
 }

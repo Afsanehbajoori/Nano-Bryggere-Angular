fcb9925ae983e5435b440062d60eb03e06e21fdb
Mikkel Gissel <mikkel@gizzel.eu>
2022-03-17 09:24:47 +0100

    meget tvivlsomt commt. Angular version beefed

--- src/app/login/login-side/login-side.component.ts
+++ src/app/login/login-side/login-side.component.ts
@@ -3,7 +3,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Bruger } from 'src/app/Models/Bruger';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestApiService } from 'src/app/shared/rest-api.service';
-
+import { LoginService } from 'src/app/shared/login.service'
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login-side',
   templateUrl: './login-side.component.html',
@@ -13,6 +15,9 @@ import { RestApiService } from 'src/app/shared/rest-api.service';
 export class LoginSideComponent implements OnInit {
   login: any = {};
   logins: Bruger[];
+  expToken: any;
+  tokenPayload: any;
+  expirationDate: any;
   endpoints = '/Logins';
   endpointK = '/Kontaktoplysninger';
   endpointB = '/Brugere';
@@ -24,60 +29,56 @@ export class LoginSideComponent implements OnInit {
   constructor(
     public router: Router,
     public restApi: RestApiService,
-    public actRoute: ActivatedRoute
+    public loginService: LoginService,
+    public actRoute: ActivatedRoute,
+    private jwtHelper: JwtHelperService
   ) { }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      brugerId: new FormControl(''),
       brugernavn: new FormControl('', Validators.required),
       pw: new FormControl('', [Validators.required, Validators.minLength(3)])
     }
     );
   }
-
-  onSubmitLogin() {
-    localStorage.clear();
-
-  this.restApi.getDatas(this.endpointB).subscribe((res) => {
-  const user = res.find((a:any) => {
-    console.log('infoLogin:' , a.kontaktoplysningerId);
-    //this.restApi.getData(a.kontaktoplysningerId , this.endpointK).subscribe(data => {
-      //console.log('infoLoginKontakt' , data);
-      return a.brugernavn.toLowerCase() === this.loginDetails.brugernavn.toLowerCase() && a.pw === this.loginDetails.pw
-   // })
-
-  });
-  if(user){
-     /* console.log("kontaktoplysningerId:",user.kontaktoplysningerId);
-     console.log("brugerId:",user.id)
-     console.log("rolleId:",user.rolleId); */
-     localStorage.setItem('kontaktoplysningerId' ,JSON.stringify(user.kontaktoplysningerId) );
-     localStorage.setItem('brugerId' ,JSON.stringify(user.id) );
-     localStorage.setItem('rolleId' ,JSON.stringify(user.rolleId) );
-     this.loginDetails.brugerId = user.id;
-     this.restApi.createData(this.loginDetails , this.endpoints).subscribe((res) => {
-      // console.log("brugerId:" ,res.brugerId);
+    onSubmitLogin(){
+      localStorage.clear()
+      this.loginService.login(this.loginForm.get('brugernavn')?.value, this.loginForm.get('pw')?.value)
+      .subscribe((response) => {
+        console.log('response on post',response)
+        this.expToken = response.bearer;
+        this.GetTokenDecoded()
+        localStorage.setItem('brugerId' , this.tokenPayload.Id);
+        localStorage.setItem('rolle' , this.tokenPayload.Role );
+        console.log("localstorage brugerId", localStorage.getItem('brugerId'))
+         
+      })
 
 
-    
-        })
-        this.router.navigate(['../main/profil']);
+     if (this.login) {
+       this.router.navigate(['../main/profil'])
+      }else{
+        alert('Bruger findes ikke')
       }
-      else {
-        alert('user ikke findes')
-      }
+    }
 
-    })
-  };
 
   onSubmitRegistre() {
     this.router.navigate(['../login/registrer']);
   };
 
-  loadLogin() {
-    return this.restApi.getData(this.login.id, this.endpoints).subscribe((logins) => {
-      this.login = logins;
+  loadLogin(){
+   // let id = this.tokenPayload.id
+    console.log('localstorage from loadLogin ', localStorage.getItem('brugerId'))
+    return this.restApi.getData(localStorage.getItem('brugerId'), this.endpointB).subscribe(data => {
+      
+      localStorage.setItem('kontaktoplysningerId', data.kontaktoplysningerId)
+      this.login = data;
+      this.logins.push(data)
     })
+  }
+  GetTokenDecoded() {
+    console.log("decoded token",this.jwtHelper.decodeToken(this.expToken))
+    this.tokenPayload = this.jwtHelper.decodeToken(this.expToken);
   }
 }
